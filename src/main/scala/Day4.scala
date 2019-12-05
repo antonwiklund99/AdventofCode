af73dd55fc594b4e3da4a87a4cc3b291bb015dd6
anton <antonwiklund199@gmail.com>
2019-12-05 10:41:39 +0100

    day 4-5

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -1,19 +1,51 @@
 object Day4 {
-  val input = scala.io.Source.fromFile("data/data4.txt", "UTF-8").getLines.toVector
-
   def part1(): Unit = {
-
+    var res = 0
+    for (i <- 168630 to 718098) {
+      var low = 0
+      var j = 0
+      var in = i.toString.split("")
+      var fail = false
+      while (!fail && j < in.length) {
+        val x = in(j).toInt
+        if (x < low) {
+          fail = true
+        }
+        low = x
+        j += 1
+      }
+      if (!fail && in.distinct.length != in.length) res += 1
+    }
+    println(res)
   }
 
   def part2(): Unit = {
-
+    var res = 0
+    for (i <- 168630 to 718098) {
+      var low = 0
+      var j = 0
+      var in = i.toString.split("").map(_.toInt)
+      var fail = false
+      var oneDouble = false
+      while (!fail && j < in.length) {
+        val x = in(j)
+        if (x < low) {
+          fail = true
+        }
+        else if (j + 1 < in.length && x == in(j + 1) && in.count(_ == x) == 2) oneDouble = true
+        low = x
+        j += 1
+      }
+      if (!fail && oneDouble) res += 1
+    }
+    println(res)
   }
 
   def apply() = {
     println("SOLUTION DAY 4")
     println("Running part 1")
-    part1()
+    //part1()
     println("Running part 2")
-    //part2()
+    part2()
   }
 }
--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -1,8 +1,54 @@
 object Day5 {
-  val input = scala.io.Source.fromFile("data/data5.txt", "UTF-8").getLines.toVector
-
   def part1(): Unit = {
+    val mem = scala.io.Source.fromFile("data/data5.txt", "UTF-8").getLines.toVector(0).split(',').map(_.toInt).toArray
+    var pc = 0
 
+    def getValues(): (Int,Int) = {
+      var isPosModes = mem(pc).toString.substring(0,math.max(mem(pc).toString.length() - 2,0)).map(_ == '0')
+      for (i <- 0 until 2 - isPosModes.length) isPosModes = true +: isPosModes
+      val i = if (isPosModes(1)) mem(mem(pc + 1)) else mem(pc + 1)
+      val j = if (isPosModes(0)) mem(mem(pc + 2)) else mem(pc + 2)
+      (i,j)
+    }
+
+    while (pc < mem.length && mem(pc) != 99) {
+      mem(pc).toString.last.toString.toInt match {
+        case 1 => {
+          val (i,j) = getValues()
+          mem(mem(pc + 3)) = i + j
+          pc += 4
+        }
+        case 2 => {
+          val (i,j) = getValues()
+          mem(mem(pc + 3)) = i * j
+          pc += 4
+        }
+        case 3 => {mem(mem(pc + 1)) = 5; pc += 2}
+        case 4 => {println(mem(mem(pc + 1))); pc += 2}
+        case 5 => {
+          val (i,j) = getValues()
+          if (i != 0) pc = j
+          else pc += 3
+        }
+        case 6 => {
+          val (i,j) = getValues()
+          if (i == 0) pc = j
+          else pc += 3
+        }
+        case 7 => {
+          val (i,j) = getValues()
+          mem(mem(pc + 3)) = if (i < j) 1 else  0
+          pc += 4
+        }
+        case 8 => {
+          val (i,j) = getValues()
+          mem(mem(pc + 3)) = if (i == j) 1 else  0
+          pc += 4
+        }
+        case _ => {println("mem(pc) = " + mem(pc)); println("instruction = " +mem(pc).toString.last.toInt); throw new Exception()}
+      }
+    }
+    mem(0)
   }
 
   def part2(): Unit = {
@@ -12,7 +58,7 @@ object Day5 {
   def apply() = {
     println("SOLUTION DAY 5")
     println("Running part 1")
-    part1()
+    println(part1())
     println("Running part 2")
     //part2()
   }
