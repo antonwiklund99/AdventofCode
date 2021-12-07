55c1ed2b6afb18692581f5d07a7cd642088508ae
Anton Wiklund <antman@420blaze.it>
2021-12-07 06:36:20 +0100

    day 7

--- 2021/day7.cpp
+++ 2021/day7.cpp
@@ -1,42 +1,46 @@
 #include <vector>
 #include <string>
-#include <map>
-#include <set>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
-#include <regex>
+#include <cmath>
 #include "util.h"
 using namespace std;
 
 vector<int> nums;
-vector<string> lines;
+int biggest = -1;
 
 void part1() {
-  int res = 0;
-  for (int i = 0; i < nums.size(); i++) {
-
+  int res = 1000000000;
+  for (int i = 0; i < biggest; i++) {
+    int sum = 0;
+    for (int j = 0; j < nums.size(); j++) {
+      sum += abs(i-nums[j]);
+    }
+    res = min(sum,res);
   }
   cout << res << endl;
 }
 
 void part2() {
-
+  int res = 1000000000;
+  for (int i = 0; i < biggest; i++) {
+    int sum = 0;
+    for (int j = 0; j < nums.size(); j++) {
+      int n = abs(i-nums[j]);
+      sum += n*(n+1)/2;
+    }
+    res = min(sum,res);
+  }
+  cout << res << endl;
 }
-
-/*
-regex pattern = regex ("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-std::smatch sm = match(pattern, line);
-*/
 
 int main() {
   std::fstream data_file ("data/data7.txt", std::ios::in);
   string line;
-  while (getline(data_file, line)) {
-    nums.push_back(stoi(line));
-    lines.push_back(line);
+  getline(data_file, line);
+  for (auto s : split(line,',')) {
+    nums.push_back(stoi(s));
+    biggest = max(stoi(s),biggest);
   }
   data_file.close();
 
