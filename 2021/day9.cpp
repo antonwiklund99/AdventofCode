e1e07b8ef4ec7e834347ff36ca7988f340fe8635
Anton Wiklund <antman@420blaze.it>
2021-12-09 06:48:26 +0100

    day 9

--- 2021/day9.cpp
+++ 2021/day9.cpp
@@ -1,48 +1,81 @@
 #include <vector>
 #include <string>
-#include <map>
-#include <set>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
-#include <regex>
-#include <cmath>
+#include <queue>
 #include "util.h"
 using namespace std;
 
-vector<int> nums;
 vector<string> lines;
 
 void part1() {
   int res = 0;
-  for (int i = 0; i < nums.size(); i++) {
-
+  for (int i = 0; i < lines.size(); i++) {
+    for (int j = 0; j < lines[i].size(); j++) {
+      if ((i == 0 || lines[i-1][j] > lines[i][j]) && (i+1 == lines.size() || lines[i+1][j] > lines[i][j]) &&
+          (j == 0 || lines[i][j-1] > lines[i][j]) && (j+1 == lines[i].size() || lines[i][j+1] > lines[i][j])) {
+        res += lines[i][j] - '0' + 1;
+      }
+    }
   }
   cout << res << endl;
 }
 
 void part2() {
-
+  bool seen[lines.size()][lines[0].size()];
+  vector<int> basins{-1,-1,-1};
+  for (int i = 0; i < lines.size(); i++) {
+    for (int j = 0; j < lines[i].size(); j++) {
+      if (!seen[i][j] && lines[i][j] != '9') {
+        int size = 1;
+        queue<pair<int,int>> q;
+        q.push(pair<int,int>(i,j));
+        seen[i][j] = true;
+        while (q.size() != 0) {
+          pair<int,int> p = q.front();
+          int pi = p.first, pj = p.second;
+          q.pop();
+          if (pi != 0 && lines[pi-1][pj] != '9' && !seen[pi-1][pj]) {
+            size++;
+            seen[pi-1][pj] = true;
+            q.push(pair<int,int>(pi-1,pj));
+          }
+          if (pi+1 != lines.size() && lines[pi+1][pj] != '9' && !seen[pi+1][pj]) {
+            size++;
+            seen[pi+1][pj] = true;
+            q.push(pair<int,int>(pi+1,pj));
+          }
+          if (pj != 0 && lines[pi][pj-1] != '9' && !seen[pi][pj-1]) {
+            size++;
+            seen[pi][pj-1] = true;
+            q.push(pair<int,int>(pi,pj-1));
+          }
+          if (pj+1 != lines[0].size() && lines[pi][pj+1] != '9' && !seen[pi][pj+1]) {
+            size++;
+            seen[pi][pj+1] = true;
+            q.push(pair<int,int>(pi,pj+1));
+          }
+        }
+        if (size > basins[2]) {
+          basins[0] = basins[1];
+          basins[1] = basins[2];
+          basins[2] = size;
+        } else if (size > basins[1]) {
+          basins[0] = basins[1];
+          basins[1] = size;
+        } else if (size > basins[0]) {
+          basins[0] = size;
+        }
+      }
+    }
+  }
+  cout << basins[0]*basins[1]*basins[2] << endl;
 }
-
-/*
-regex pattern = regex ("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-std::smatch sm = match(pattern, line);
-*/
 
 int main() {
   std::fstream data_file ("data/data9.txt", std::ios::in);
   string line;
-  /*
-  getline(data_file, line);
-  for (auto s : split(line,',')) {
-    nums.push_back(stoi(s));
-  }
-  */
   while (getline(data_file, line)) {
-    nums.push_back(stoi(line));
     lines.push_back(line);
   }
   data_file.close();
