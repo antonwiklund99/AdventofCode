5bdc35b8acd075d221b53913866402b54ef9e1b2
Anton Wiklund <antman@420blaze.it>
2021-12-13 08:59:43 +0100

    day 13

--- 2021/day13.cpp
+++ 2021/day13.cpp
@@ -15,38 +15,109 @@
 #include "util.h"
 using namespace std;
 
-vector<int> nums;
-vector<string> lines;
+vector<pair<int,int>> points;
+vector<pair<char,int>> folds;
+int maxX = 0, maxY = 0;
 
 void part1() {
+  bool grid[maxY+1][maxX+1];
+  int x = maxX, y = maxY;
+  for (int i = 0; i <= y; i++) {
+    for (int j = 0; j <= x; j++) {
+      grid[i][j] = false;
+    }
+  }
+  for (auto p : points) {
+    grid[p.second][p.first] = true;
+  }
+  pair<char,int> fold = folds[0];
+  if (fold.first == 'x') {
+    for (int r = 0; r <= y; r++) {
+      for (int i = 1; i <= min(x - fold.second, fold.second); i++) {
+        int rx = fold.second + i;
+        int lx = fold.second - i;
+        grid[r][lx] = grid[r][lx] || grid[r][rx];
+      }
+    }
+    x = fold.second - 1;
+  } else {
+    for (int c = 0; c <= x; c++) {
+      for (int i = 1; i <= min(y - fold.second, fold.second); i++) {
+        int dy = fold.second + i;
+        int uy = fold.second - i;
+        grid[uy][c] = grid[uy][c] || grid[dy][c];
+      }
+    }
+    y = fold.second - 1;
+  }
   int res = 0;
-  for (int i = 0; i < lines.size(); i++) {
-
+  for (int i = 0; i <= y; i++) {
+    for (int j = 0; j <= x; j++) {
+      if (grid[i][j]) res++; 
+    }
   }
   cout << res << endl;
 }
 
 void part2() {
-
+  bool grid[maxY+1][maxX+1];
+  int x = maxX, y = maxY;
+  for (int i = 0; i <= y; i++) {
+    for (int j = 0; j <= x; j++) {
+      grid[i][j] = false;
+    }
+  }
+  for (auto p : points) {
+    grid[p.second][p.first] = true;
+  }
+  for (auto fold : folds) {
+    if (fold.first == 'x') {
+      for (int r = 0; r <= y; r++) {
+        for (int i = 1; i <= min(x - fold.second, fold.second); i++) {
+          int rx = fold.second + i;
+          int lx = fold.second - i;
+          grid[r][lx] = grid[r][lx] || grid[r][rx];
+        }
+      }
+      x = fold.second - 1;
+    } else {
+      for (int c = 0; c <= x; c++) {
+        for (int i = 1; i <= min(y - fold.second, fold.second); i++) {
+          int dy = fold.second + i;
+          int uy = fold.second - i;
+          grid[uy][c] = grid[uy][c] || grid[dy][c];
+        }
+      }
+      y = fold.second - 1;
+    }
+  }
+  for (int i = 0; i <= y; i++) {
+    for (int j = 0; j <= x; j++) {
+      cout << (grid[i][j] ? "#" : " ");
+    }
+    cout << endl;
+  }
 }
-
-/*
-regex pattern = regex ("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-std::smatch sm = match(pattern, line);
-*/
 
 int main() {
   std::fstream data_file ("data/data13.txt", std::ios::in);
   string line;
-  /*
-  getline(data_file, line);
-  for (auto s : split(line,',')) {
-    nums.push_back(stoi(s));
-  }
-  */
+  bool parsingCords = true;
   while (getline(data_file, line)) {
-    nums.push_back(stoi(line));
-    lines.push_back(line);
+    if (line.size() == 0) {
+      parsingCords = false;
+    } else if (parsingCords) {
+      vector<string> splitted = split(line,',');
+      int x = stoi(splitted[0]);
+      int y = stoi(splitted[1]);
+      maxX = max(maxX, x);
+      maxY = max(maxY, y);
+      points.push_back(pair<int,int>(x,y));
+    } else {
+      regex pattern = regex ("fold along (x|y)=(\\d+)");
+      std::smatch sm = match(pattern, line);
+      folds.push_back(pair<char,int>(sm[1] == "x" ? 'x' : 'y', stoi(sm[2])));
+    }
   }
   data_file.close();
 
