1f4dafbe1dc33736c752879445a465e20e413066
Anton Wiklund <antman@420blaze.it>
2021-12-15 07:21:16 +0100

    day 15

--- 2021/day15.cpp
+++ 2021/day15.cpp
@@ -1,51 +1,143 @@
 #include <vector>
 #include <string>
-#include <map>
-#include <set>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
-#include <regex>
-#include <cmath>
-#include <cctype>
 #include <queue>
-#include <stack>
 #include "util.h"
 using namespace std;
 
-vector<int> nums;
 vector<string> lines;
 
-void part1() {
-  int res = 0; 
-  for (int i = 0; i < lines.size(); i++) {
+struct Node {
+  int risk;
+  pair<int,int> cords;
+};
 
+class NodeCompare {
+  public:
+    bool operator() (Node a, Node b) {
+      return a.risk > b.risk;
+    }
+};
+
+void part1() {
+  bool visited[lines.size()][lines[0].size()];
+  for (int i = 0; i < lines.size();i++) {
+    for (int j = 0; j < lines[0].size();j++) {
+      visited[i][j] = false;
+    }
   }
-  cout << res << endl;
+  priority_queue<Node, vector<Node>, NodeCompare> q;
+  q.push(Node{0,pair<int,int>(0,0)});
+  while (true) {
+    Node p = q.top();
+    pair<int,int> cords = p.cords;
+    q.pop();
+    if (cords.first == lines[0].size()-1 && cords.second == lines.size()-1) {
+      cout << p.risk << endl;
+      return;
+    }
+    if (visited[cords.second][cords.first]) continue;
+    visited[cords.second][cords.first] = true;
+    if (cords.second - 1 > 0 && !visited[cords.second-1][cords.first]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first,cords.second-1);
+      n.risk = p.risk + lines[cords.second-1][cords.first] - '0';
+      q.push(n);
+    }
+    if (cords.second + 1 < lines.size() && !visited[cords.second+1][cords.first]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first,cords.second+1);
+      n.risk = p.risk + lines[cords.second+1][cords.first] - '0';
+      q.push(n);
+    }
+    if (cords.first - 1 > 0 && !visited[cords.second][cords.first-1]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first-1,cords.second);
+      n.risk = p.risk + lines[cords.second][cords.first-1] - '0';
+      q.push(n);
+    }
+    if (cords.first + 1 < lines[0].size() && !visited[cords.second][cords.first+1]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first+1,cords.second);
+      n.risk = p.risk + lines[cords.second][cords.first+1] - '0';
+      q.push(n);
+    }
+  }
 }
 
 void part2() {
-
+  int R = lines.size()*5;
+  int C = lines[0].size()*5;
+  int OR = lines.size();
+  int OC = lines[0].size();
+  int grid[R][C];
+  bool visited[R][C];
+  for (int i = 0; i < R;i++) {
+    for (int j = 0; j < C;j++) {
+      visited[i][j] = false;
+    }
+  }
+  for (int r = 0; r < 5; r++) {
+    for (int c = 0; c < 5; c++) {
+      for (int i = 0; i < OR; i++) {
+        for (int j = 0; j < OC; j++) {
+          if (r == 0 && c == 0) {
+            grid[i][j] = lines[i][j]-'0';
+          } else if (r == 0) {
+            grid[i][j+c*OC] = grid[i][j+(c-1)*OC]+1;
+            if (grid[i][j+c*OC] > 9) grid[i][j+c*OC] = 1;
+          } else {
+            grid[i+r*OR][j+c*OC] = grid[i+(r-1)*OR][j+c*OC]+1;
+            if (grid[i+r*OR][j+c*OC] > 9) grid[i+r*OR][j+c*OC] = 1;
+          }
+        }
+      }
+    }
+  }
+  priority_queue<Node, vector<Node>, NodeCompare> q;
+  q.push(Node{0,pair<int,int>(0,0)});
+  while (true) {
+    Node p = q.top();
+    pair<int,int> cords = p.cords;
+    q.pop();
+    if (cords.first == C-1 && cords.second == R-1) {
+      cout << p.risk << endl;
+      return;
+    }
+    if (visited[cords.second][cords.first]) continue;
+    visited[cords.second][cords.first] = true;
+    if (cords.second - 1 > 0 && !visited[cords.second-1][cords.first]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first,cords.second-1);
+      n.risk = p.risk + grid[cords.second-1][cords.first];
+      q.push(n);
+    }
+    if (cords.second + 1 < R && !visited[cords.second+1][cords.first]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first,cords.second+1);
+      n.risk = p.risk + grid[cords.second+1][cords.first];
+      q.push(n);
+    }
+    if (cords.first - 1 > 0 && !visited[cords.second][cords.first-1]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first-1,cords.second);
+      n.risk = p.risk + grid[cords.second][cords.first-1];
+      q.push(n);
+    }
+    if (cords.first + 1 < C && !visited[cords.second][cords.first+1]) {
+      Node n;
+      n.cords = pair<int,int>(cords.first+1,cords.second);
+      n.risk = p.risk + grid[cords.second][cords.first+1];
+      q.push(n);
+    }
+  }
 }
-
-/*
-regex pattern = regex ("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-std::smatch sm = match(pattern, line);
-*/
 
 int main() {
   std::fstream data_file ("data/data15.txt", std::ios::in);
   string line;
-  /*
-  getline(data_file, line);
-  for (auto s : split(line,',')) {
-    nums.push_back(stoi(s));
-  }
-  */
   while (getline(data_file, line)) {
-    nums.push_back(stoi(line));
     lines.push_back(line);
   }
   data_file.close();
