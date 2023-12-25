65b4649b1642f0fc4a65ae49b8cda3734b9df73f
Anton Wiklund <antman420@blaze.it>
2023-12-25 13:55:32 +0100

    2023 day 25 (without 'import solution')

--- 2023/day25.py
+++ 2023/day25.py
@@ -1,21 +1,43 @@
 #!/usr/bin/pypy3
-import networkx as nx
+from collections import defaultdict
+import random
 
 a = []
 with open("data/data25", "r") as f:
   for l in f:
     a.append(l.strip())
 
-g = nx.Graph()
+# https://en.wikipedia.org/wiki/Karger%27s_algorithm
+def kargers(eo):
+  g = [set([k]) for k in eo.keys()]
+  e = [set(eo[k]) for k in eo.keys()]
+  while len(g) > 2:
+    i = random.randint(0,len(g)-1)
+    n = random.sample(e[i],1)[0]
+    j = 0
+    while not n in g[j]:
+      j += 1
+    g[i] = g[i].union(g[j])
+    e[i] = set(n for n in e[i].union(e[j]) if not n in g[i])
+    g.pop(j)
+    e.pop(j)
+  return g
+    
+e = defaultdict(list)
 for l in a:
   [src,dsts] = l.split(": ")
   src = src.strip()
   for dst in dsts.strip().split(" "):
-    g.add_edge(src,dst)
+    e[src].append(dst)
+    e[dst].append(src)
 
-for e in nx.minimum_edge_cut(g):
-  g.remove_edge(*e)
-res = 1
-for c in nx.connected_components(g):
-  res *= len(c)
-print(res)
+while True:
+  g = kargers(e)
+  er = []
+  for n1 in g[0]:
+    for n2 in g[1]:
+      if n1 in e[n2]:
+        er.append((n1,n2))
+  if len(er) <= 3:
+    print(len(g[0])*len(g[1]))
+    break
